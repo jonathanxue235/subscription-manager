8c099ac610da5382cdd7a033325b17cff3e61e99
Lasereyepop <ziyangzhou2003@gmail.com>
2025-12-03 22:19:25 -0800

    Added frontend validation for password and email in SignUp

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -64,8 +64,24 @@ class AuthService {
       throw new Error('Password is required');
     }
 
-    if (password.length < 6) {
-      throw new Error('Password must be at least 6 characters');
+    if (password.length < 8) {
+      throw new Error('Password must be at least 8 characters');
+    }
+
+    if (!/[A-Z]/.test(password)) {
+      throw new Error('Password must contain at least one uppercase letter');
+    }
+
+    if (!/[a-z]/.test(password)) {
+      throw new Error('Password must contain at least one lowercase letter');
+    }
+
+    if (!/[0-9]/.test(password)) {
+      throw new Error('Password must contain at least one number');
+    }
+
+    if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
+      throw new Error('Password must contain at least one special character (!@#$%^&*(),.?":{}|<>)');
     }
   }
 }
