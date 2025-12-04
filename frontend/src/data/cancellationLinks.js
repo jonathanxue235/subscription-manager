be443ee45897186f5fb9ddf3d7a8e7d10f9d271d
whilehak <whilehak@gmail.com>
2025-12-03 22:39:49 -0800

    Added more subscription icons and cancellation links

--- frontend/src/data/cancellationLinks.js
+++ frontend/src/data/cancellationLinks.js
@@ -1,5 +1,7 @@
 const cancellationLinks = {
     amazonprime: 'https://www.amazon.com/mm/pipeline/cancellation',
+    appletv: 'https://support.apple.com/en-us/118398',
+    disneyplus: 'https://www.disneyplus.com/account/cancel-subscription',
     hulu: 'https://help.hulu.com/article/hulu-cancel-hulu-subscription',
     netflix: 'https://www.netflix.com/cancelplan',
     spotify: 'https://www.spotify.com/us/signed-out/cancel/',
